42967c6ebd56ec6fb6d1bd8a76230e2932fbd366
samoTh137 <78793115+samoTh137@users.noreply.github.com>
2022-05-21 15:45:14 +0200

    Update OpenQuestion.dart

--- geo_exam/lib/User/Questions/OpenQuestion.dart
+++ geo_exam/lib/User/Questions/OpenQuestion.dart
@@ -3,9 +3,12 @@ import 'package:firebase_database/firebase_database.dart';
 
 String questionString = 'Open vraag komt hier';
 String _antwoord = '';
-
+String _error = '';
 
 class OpenQuestion extends StatefulWidget {
+  final String? currentUser;
+  const OpenQuestion({Key? key, required String? this.currentUser}) : super(key: key);
+
   @override
   _OpenQuestionState createState() => _OpenQuestionState();
 }
@@ -45,13 +48,91 @@ class _OpenQuestionState extends State<OpenQuestion> {
                     ),
                   )),
             ]),
-        body: Center(
-            child: Text(
-              questionString,
-              textAlign: TextAlign.center,
-              style: const TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+        body: Column(
+          children: [
+            Row(
+              children:[
+                Text(
+                  questionString,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                )
+              ],
+            ),
+            Row(
+              children: [
+                Flexible(child:
+                Padding(
+                  padding: EdgeInsets.all(24),
+                  child: TextFormField(
+                      decoration: const InputDecoration(
+                        border: UnderlineInputBorder(),
+                        labelText: 'Voer het antwoord in',
+                      ),
+                      onChanged: (value) {
+                        setState(() {
+                          _antwoord = value;
+                        });
+                      }),
+                ),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Flexible(child :
+                Padding(
+                  padding: const EdgeInsets.all(24.0),
+                  child: ElevatedButton(
+                    onPressed: () async {
+                      FirebaseDatabase.instance.ref("Vragen/OpenVragen/$_vraag/$_user/Antwoord") //Set naar de juiste firebase "Tak" (variabele zodat het een nieuwe tak maakt)
+                          .set({ //Maak het stuk JSON
+                        " Antwoord": {_antwoord}
+                      })
+                          .then((_) {_error = "Successfully uploaded Question!";showErrorDialog(context);}) //Dialog voor success
+                          .catchError((error) {_error = "An error occured! Please try again.";showErrorDialog(context);}); //Dialog voor error
+                    },
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.red[800], // set the background color
+                      fixedSize: const Size(240,80),
+                    ),
+                    child: const Text('Vraag toevoegen'),
+                  ),
+                ),
+                ),
+              ],
             )
+          ],
+
         )
     );
   }
+}
+
+showErrorDialog(BuildContext context) {
+
+  // set up the button
+  Widget okButton = TextButton(
+    child: Text("OK"),
+    onPressed: () {
+      Navigator.of(context).pop();
+    },
+  );
+
+  // set up the AlertDialog
+  AlertDialog alert = AlertDialog(
+    title: Text("Something happened!"),
+    content: Text(_error),
+    actions: [
+      okButton,
+    ],
+  );
+
+  // show the dialog
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return alert;
+    },
+  );
 }
